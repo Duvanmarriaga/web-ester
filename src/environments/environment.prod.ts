2232bad188a2a4c885b955c68685e7d148a5b16e
Keiner Molina Lemus <keinermolinalemus@MacBook-Pro-de-Keiner.local>
2025-12-02 23:12:38 -0500

    changes url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // Environment for Production
 export const environment = {
   production: true,
-  apiUrl: 'http://3.146.226.16/api', // URL base para desarrollo
+  apiUrl: 'https://3.146.226.16/api', // URL base para desarrollo
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 // Environment for Development
 export const environment = {
   production: false,
-  apiUrl: 'http://3.146.226.16/api', // URL base para desarrollo
+  apiUrl: 'https://3.146.226.16/api', // URL base para desarrollo
 };
 
